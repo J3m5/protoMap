fd115453fcd759b1ea650084c0d2f101a87eafa0
j3m5 <je.hu@protonmail.com>
2018-03-16 10:58:13 +0100

    first commit

---
+++ client/map.js
@@ -0,0 +1,106 @@
+let map;
+let coords = [];
+let flightPath;
+const socket = io.connect('http://localhost:3000');
+
+socket.on('update', function (data) {
+    let position = JSON.parse(data.message.payload);
+    console.log(position);
+    $('tbody').prepend(
+        `<tr>
+            <td>${position.data.id}</td>
+            <td>${position.data.pos.lat}</td>
+            <td>${position.data.pos.long}</td>
+        </tr>`);
+    map.panTo({lat : position.data.pos.lat, lng: position.data.pos.long});
+    let path = flightPath.getPath();
+    path.push(new google.maps.LatLng(position.data.pos.lat, position.data.pos.long));
+    flightPath.setPath(path);
+    if(flightPath.getPath().length === 1){
+        map.setZoom(18);
+    }
+});
+
+function initMap() {
+    map = new google.maps.Map($('.map')[0], {
+        center: {lat: 44.05255, lng: 4.137},
+        zoom: 8
+    });
+    flightPath = new google.maps.Polyline({
+        path: [],
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+
+    flightPath.setMap(map);
+}
+
+const emptyNode = (node) => {
+    node = $(node)[0];
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+};
+
+const getPositions = () => {
+
+    $.get('http://localhost:3000/request')
+        .done(function (data) {
+            emptyNode('tbody');
+            data.forEach((el) => {
+                coords.push({lat: el.position.lat, lng: el.position.long});
+                $('tbody').append(
+                    `<tr>
+                        <td>${el.id}</td>
+                        <td>${el.position.lat}</td>
+                        <td>${el.position.long}</td>
+                    </tr>`)
+            });
+            console.log(data);
+        })
+        .fail(function (err) {
+            console.log(err);
+        })
+};
+
+const getPositionsOnCLick = () => {
+    $(".btn.getPositions").click(function () {
+        console.log('click');
+        getPositions();
+    })
+};
+
+const postPositionsOnCLick = () => {
+    $(".btn.startInsert").click(function () {
+        console.log('click');
+        $.post('http://localhost:3000/startInsert')
+            .fail( (err) => {
+                console.log(err);
+            })
+    })
+};
+
+const deletePositionsOnCLick = () => {
+    $(".btn.deleteData").click(function () {
+        console.log('click');
+        $.post('http://localhost:3000/delete_data')
+            .done( () => {
+                emptyNode('tbody');
+                flightPath.getPath().clear();
+            })
+            .fail( (err) => {
+                console.log(err);
+            })
+    })
+};
+
+$(function () {
+
+    getPositionsOnCLick();
+    postPositionsOnCLick();
+    deletePositionsOnCLick();
+});
+
+// AIzaSyCEH43Unag1nRoqq2v4aiYAfAjOCJ_Q2ig
---
+++ server/app.js
@@ -0,0 +1,133 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const pgp = require('pg-promise')();
+const io = require('socket.io')(server);
+const positionsJson = require('./latlong.json');
+const coordinates = positionsJson.features[0].geometry.coordinates;
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+server.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+const connection = {
+    user: 'aqeynyrq',
+    host: 'packy.db.elephantsql.com',
+    database: 'aqeynyrq',
+    password: 'QYp4irxpPwmB8Q3iWhgqWqIrJCTW1P5c',
+    port: 5432,
+};
+
+const db = pgp(connection);
+
+db.none(`CREATE OR REPLACE FUNCTION notify_trigger() RETURNS trigger AS $$ 
+            DECLARE 
+                data json;
+                notification json;
+            BEGIN 
+            
+                data = row_to_json(NEW);
+                notification = json_build_object(
+                  'table',TG_TABLE_NAME,
+                  'action', TG_OP,
+                  'data', data);
+                PERFORM pg_notify('watchers', notification::text );
+                RETURN NULL;
+            END;
+        $$ LANGUAGE plpgsql;`)
+    .then(() => {
+        // success;
+        console.log('success')
+    })
+    .catch(error => {
+        // error;
+        console.log(error);
+    });
+
+db.any('select tgname from pg_trigger;')
+    .then((data) => {
+        // success;
+        console.log(data);
+        if (data.length === 0 || typeof data[0].tgname !== "string" ||  data[0].tgname !== "watched_table_trigger") {
+            db.none(`CREATE TRIGGER watched_table_trigger AFTER INSERT ON positions
+                        FOR EACH ROW EXECUTE PROCEDURE notify_trigger();`)
+                .then(() => {
+                    // success;
+                    console.log('success')
+                })
+                .catch(error => {
+                    // error;
+                    console.log(error);
+                });
+        }
+    })
+    .catch(error => {
+        // error;
+        console.log(error);
+    });
+
+
+
+    db.connect({direct: true})
+        .then(
+            (sco) => {
+                io.on('connection', function (socket) {
+                    console.log('socket connected');
+                    sco.client.on('notification', data => {
+                console.log('Received:', data);
+                socket.emit('update', { message: data });
+                // data.payload = 'my payload string'
+                    });
+            });
+                return sco.none('LISTEN $1~', 'watchers');
+            })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/request', async (req, res) => {
+    try {
+        const result = await db.any("SELECT id AS ID, pos AS position FROM positions");
+        console.log(result);
+        res.send(result);
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.post('/delete_data', async (req, res) => {
+    try {
+        const result = await db.any("TRUNCATE TABLE positions RESTART IDENTITY;");
+        console.log(result);
+        res.send(result);
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+
+app.post('/startInsert', (req, res) => {
+    let i = 0;
+    let interval = setInterval(async function () {
+        console.log(i);
+        try {
+            const result = await db.one('INSERT INTO positions(pos) VALUES($1) RETURNING id', ['{"lat": ' + coordinates[i][1] + ', "long": ' + coordinates[i][0] + '}']);
+            ++i;
+            if (i === 100) {
+                clearInterval(interval);
+            }
+            console.log(result);
+            // res.send(result);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }, 1000);
+});
