f566bf43966fde6665d51af513a8682dbd26e259
Home <alphalight@protonmail.com>
2018-03-17 02:22:40 +0100

    nex test socket url

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const app = require('express')();
 const fs = require('fs');
 
 const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/coda-jr.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/coda-jr.com/fullchain.pem'),
+    key: fs.readFileSync('privkey.pem'),
+    cert: fs.readFileSync('fullchain.pem'),
     requestCert: true
 };
 
